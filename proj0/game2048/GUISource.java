6c8f5fe34ce45a314388eac3b384eab6286e69b3
zhz_vite mac <2811215248@qq.com>
2024-11-07 09:51:06 +0800

    完成pro0，调整了一个上下界，判了下边界就过了

--- proj0/game2048/GUISource.java
+++ proj0/game2048/GUISource.java
@@ -35,7 +35,8 @@ class GUISource implements InputSource {
     public String getKey() {
         String command = _source.readKey();
         switch (command) {
-            case "向上箭头" :
+        //    case "向上箭头" :
+            case "↑":
                 command = "Up";
                 break;
             case "→" :
